07d3ae56a7372d43727a1b3154537b2ac0735791
SmoeJith <josephsmith@my.ccsu.edu>
2022-12-12 02:36:13 -0500

    Added a nice user profile view, refactored some python-noncompliant names, and implemented custom behavior for temporarily disabling buttons through HTML data attributes

--- cosmos/urls.py
+++ cosmos/urls.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.homePage, name='landing'),
-    path('post/create', views.createPost, name='createPost'),
-    path('post/reply/<int:other_post>', views.createReply, name='createReply'),
-    path('post/<int:view_post>', views.showPost, name='queryPost'),
-    path('user/<int:other_user>', views.showProfile, name='queryUser'),
-    path('user/', views.showProfile, name='queryUser'),
+    path('', views.home, name='landing'),
+    path('post/create', views.create_post, name='create_post'),
+    path('post/reply/<int:other_post>', views.create_reply, name='create_reply'),
+    path('post/<int:view_post>', views.show_post, name='queryPost'),
+    path('user/<int:profile_user>', views.user_profile, name='queryUser'),
+    path('user/', views.user_profile, name='queryUser'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
 if(settings.DEBUG):
--- cosmos/views.py
+++ cosmos/views.py
@@ -9,45 +9,48 @@
 #    return render(request, 'cosmos/user-profile.html', context)
 from django.shortcuts import render, redirect
 from .models import Post
-from CS416FinalProject.forms import UpdateUserForm, CreateUserForm, CreatePostForm
+from CS416FinalProject.forms import UpdateUserForm, CreateUserForm, create_post_form
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='login')
-def homePage(request):
+def home(request):
     context = {'posts': Post.objects.filter(user=request.user).order_by('posted_at').reverse(),
-               'current_user': request.user}
+               'current_user': request.user,
+               'profile_user': request.user
+               }
     return render(request, 'cosmos/user-profile.html', context)
 
 
-def showProfile(request, other_user=None):
-    if request.method == 'POST' or other_user == request.user.id or other_user is None:
+def user_profile(request, profile_user=None):
+    if request.method == 'POST' or profile_user == request.user.id or profile_user is None:
         redirect('/')
-    context = {'posts': Post.objects.filter(user=other_user).order_by('posted_at').reverse(),
-               'current_user': request.user}
+    context = {'posts': Post.objects.filter(user=profile_user).order_by('posted_at').reverse(),
+               'current_user': request.user,
+               'profile_user': User.objects.get(id=profile_user).profile.user}
     return render(request, 'cosmos/user-profile.html', context)
 
 
 @login_required(login_url='login')
-def createPost(request):
+def create_post(request):
     if request.method == 'POST':
-        form = CreatePostForm(request.POST)
+        form = create_post_form(request.POST)
         if form.is_valid():
             user = request.user
             post_body = form.cleaned_data['post_body']
             post = Post(user=user, post_body=post_body)
             post.save()
             return redirect('/')
-    context = {'form': CreatePostForm(),
+    context = {'form': create_post_form(),
                'header_flavor': 'Create a Post',
                'button_flavor': 'Go!'}
     return render(request, 'cosmos/create-post.html', context)
 
 
 @login_required(login_url='login')
-def showPost(request, view_post=None):
+def show_post(request, view_post=None):
     if request.method == 'POST' or view_post is None:
         return redirect('/')
     context = {'posts': Post.objects.filter(id=view_post).union(Post.objects.filter(reply_id=view_post)),
@@ -55,18 +58,18 @@ def showPost(request, view_post=None):
     return render(request, 'cosmos/post-thread.html', context)
 
 @login_required(login_url='login')
-def createReply(request, other_post=None):
+def create_reply(request, other_post=None):
     if other_post is None:
         return redirect('landing')
     if request.method == 'POST':
-        form = CreatePostForm(request.POST)
+        form = create_post_form(request.POST)
         if form.is_valid():
             user = request.user
             post_body = form.cleaned_data['post_body']
             post = Post(reply_id=other_post, user=user, post_body=post_body)
             post.save()
             return redirect('/')
-    context = {'form': CreatePostForm(),
+    context = {'form': create_post_form(),
                'header_flavor': 'Create a Post',
                'button_flavor': 'Go!'}
     return render(request, 'cosmos/create-post.html', context)
